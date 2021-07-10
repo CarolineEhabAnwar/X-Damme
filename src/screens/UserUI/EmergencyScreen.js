6dc74b093f2950ad65555dec73d48980809c1f05
Caroline Ehab <68237645+CarolineEhabAnwar@users.noreply.github.com>
2021-07-10 14:47:26 +0200

    we keep adjusting

--- src/screens/UserUI/EmergencyScreen.js
+++ src/screens/UserUI/EmergencyScreen.js
@@ -4,78 +4,68 @@ import { Container, Badge, FooterTab, Header, Button, Text, Icon, InputGroup, In
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Entypo, FontAwesome, MaterialIcons, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Fontisto, Ionicons } from '@expo/vector-icons';
-import { DrawerActions } from 'react-navigation-drawer';
+import FooterComponent from "../components/FooterComponent"
 
-export default class EmergencyScreen extends Component {
-  render() {
-    let home_notification = 5;
-    let profile_notification = 5;
-    let settings_notification = 5;
-    return (
-      <Container>
-        {/* Text with navback */}
-        <View searchBar style={{ flexDirection: 'row', paddingTop: 25, paddingBottom: 6, alignContent: "center", backgroundColor: "darkred", top: 0 }}>
-          <Button transparent onPress={() => this.props.navigation.navigate('Home')} >
-            <Ionicons
-              name='arrow-back-outline'
-              style={{ fontSize: 30, marginTop: 4, marginRight: 12, marginLeft: 12, color: 'white' }}
-            />
+const EmergencyScreen = ({ navigation }) => {
+
+  return (
+    <Container>
+      {/* Text with navback */}
+      <View searchBar style={{ flexDirection: 'row', paddingTop: 25, paddingBottom: 6, alignContent: "center", backgroundColor: "darkred", top: 0 }}>
+        <Button transparent onPress={() => navigation.navigate('Home')} >
+          <Ionicons
+            name='arrow-back-outline'
+            style={{ fontSize: 30, marginTop: 4, marginRight: 12, marginLeft: 12, color: 'white' }}
+          />
+        </Button>
+        <Text style={{ color: "white", height: 50, fontSize: 20, textAlign: 'center', paddingLeft: '22%', paddingTop: 12, fontWeight: 'bold' }}>Emergency</Text>
+      </View>
+      {/* End Text with navback */}
+      <Grid>
+        <Col style={{ backgroundColor: '#c99c1e' }}>
+          <Button full transparent style={styles.buttonStyle} onPress={() => navigation.navigate('PingMap')}>
+            <Entypo style={styles.iconStyle} name="location-pin" />
+            <Text style={styles.textStyle}>Ping Nearest Users</Text>
           </Button>
-          <Text style={{ color: "white", height: 50, fontSize: 20, textAlign: 'center', paddingLeft: '22%', paddingTop: 12, fontWeight: 'bold' }}>Emergency</Text>
-        </View>
-        {/* End Text with navback */}
-        <Grid>
-          <Col style={{ backgroundColor: '#c99c1e' }}>
-            <Button full transparent style={styles.buttonStyle} onPress={() => this.props.navigation.navigate('PingMap')}>
-              <Entypo style={styles.iconStyle} name="location-pin" />
-              <Text style={styles.textStyle}>Ping Nearest Users</Text>
-            </Button>
-          </Col>
-          <Col style={{ backgroundColor: '#1b2696' }}>
-            <Button full transparent style={styles.buttonStyle} onPress={() => this.props.navigation.navigate('EmergencyContacts')} >
-              <MaterialIcons name="quick-contacts-dialer" style={styles.iconStyle} />
-              <Text style={styles.textStyle}>Emergency Contacts </Text>
-            </Button>
-          </Col>
-        </Grid>
-        <Grid>
-          <Col style={{ backgroundColor: '#d91118' }}>
-            <Button full transparent style={styles.buttonStyle}>
-              <FontAwesome5 style={styles.iconStyle} name="hospital" />
-              <Text style={styles.textStyle}>Nearby Hospitals</Text>
-            </Button>
-          </Col>
-          <Col style={{ backgroundColor: '#80207e' }}>
-            <Button full transparent style={styles.buttonStyle} onPress={() => this.props.navigation.navigate('WinchNumbers')}>
-              <MaterialCommunityIcons name="tow-truck" style={styles.iconStyle} />
-              <Text style={styles.textStyle}>Winch Numbers</Text>
-            </Button>
-          </Col>
-        </Grid>
-        {/* Footer */}
-        <View style={{ flexDirection: 'row', alignContent: "center", backgroundColor: "darkred" }}>
-          <FooterTab transparent style={{ backgroundColor: "darkred" }}>
-            <Button style={{ marginTop: 5 }} onPress={() => this.props.navigation.navigate('Home')}>
-              <Icon style={{ color: 'white' }} name="home" />
-              <Text style={{ color: 'white' }}> Home</Text>
-            </Button>
+        </Col>
+        <Col style={{ backgroundColor: '#1b2696' }}>
+          <Button full transparent style={styles.buttonStyle} onPress={() => navigation.navigate('EmergencyContacts')} >
+            <MaterialIcons name="quick-contacts-dialer" style={styles.iconStyle} />
+            <Text style={styles.textStyle}>Emergency Contacts </Text>
+          </Button>
+        </Col>
+      </Grid>
+      <Grid>
+        <Col style={{ backgroundColor: '#d91118' }}>
+          <Button full transparent style={styles.buttonStyle} onPress={() => {
+            navigation.navigate("NearbyHospitals")
+            console.log("Hospitals")
+          }}>
+            <FontAwesome5 style={styles.iconStyle} name="hospital" />
+            <Text style={styles.textStyle}>Nearby Hospitals</Text>
+          </Button>
+        </Col>
+        <Col style={{ backgroundColor: '#80207e' }}>
+          <Button full transparent style={styles.buttonStyle} onPress={() => navigation.navigate('WinchNumbers')}>
+            <MaterialCommunityIcons name="tow-truck" style={styles.iconStyle} />
+            <Text style={styles.textStyle}>Winch Numbers</Text>
+          </Button>
+        </Col>
+      </Grid>
 
-            <Button style={{ marginTop: 5 }} onPress={() => this.props.navigation.navigate('Profile')}>
-              <Icon name="person" style={{ color: 'white' }} />
-              <Text style={{ color: 'white' }}>Profile</Text>
-            </Button>
+      <FooterComponent
+        home="Home"
+        profile="Profile"
+        contactus="ContactUs"
+        bkcolor="darkred"
+      />
 
-            <Button style={{ marginTop: 5 }} onPress={() => this.props.navigation.navigate('ContactUs')}>
-              <Icon style={{ color: 'white' }} name="call" />
-              <Text style={{ color: 'white' }} >Contact Us</Text>
-            </Button>
-          </FooterTab>
-        </View>
-        {/* End Footer */}
-      </Container>
-    );
-  }
+    </Container>
+  );
 }
+
+export default EmergencyScreen;
+
 const styles = StyleSheet.create({
   buttonStyle: {
     width: "100%",
